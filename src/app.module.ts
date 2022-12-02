1fbf40360f6c26ad54464c883c011eab9adba45f
jrhe1213 <jiarong1213@gmail.com>
2022-12-01 23:13:04 -0500

    joi validate ENV variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+// config
 import { ConfigModule } from '@nestjs/config';
 import configuration from './configuration';
+import * as joi from 'joi';
+// modules
+import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
+      validationSchema: joi.object({
+        NODE_ENV: joi
+          .string()
+          .valid('development', 'staging', 'production')
+          .default('development'),
+      }),
     }),
     UserModule,
   ],
